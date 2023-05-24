8fdfb2ddcb42ae08116791a8db1ff5f37fa3fd33
SadanandaJaya <sadananda.jaya@gmail.com>
2023-05-24 16:31:32 +0530

    base class change

--- src/events/base-classes/base-publisher.ts
+++ src/events/base-classes/base-publisher.ts
@@ -17,6 +17,7 @@ export abstract class Publisher<T extends Event> {
   publish(data: T["data"]): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        console.log("Event published", this.subject);
         if (err) {
           return reject(err);
         }
